14104efab26e2216eb907083783fb3375672907d
Yeho Gwon <devstita@gmail.com>
2023-07-14 15:45:05 +0900

    implement lienar layer

--- common/layer.py
+++ common/layer.py
@@ -17,3 +17,20 @@ class Layer(metaclass=ABCMeta):
 
     def __call__(self, *args: Any, **kwds: Any) -> Any:
         return self.forward(*args, **kwds)
+
+class Linear(Layer): 
+    def __init__(self, input_dim: int, output_dim: int) -> None:
+        self.W = np.random.randn(output_dim, input_dim)
+        self.b = np.random.randn(output_dim, 1)
+
+    def forward(self, x: np.ndarray) -> np.ndarray: 
+        self.x = x
+        return self.W @ x + self.b
+
+    def backward(self, grad: float, optimizer: Callable) -> float: 
+        self.w_grad = grad @ self.x.T
+        self.b_grad = grad
+        self.x_grad = self.W.T @ grad
+        self.W = optimizer(self.W, self.w_grad)
+        self.b = optimizer(self.b, self.b_grad)
+        return self.x_grad
