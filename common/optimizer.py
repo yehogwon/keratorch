32d6260c2a81dbe0b7034f2b9655651bc141f18c
Yeho Gwon <devstita@gmail.com>
2023-07-14 15:26:31 +0900

    callable optimizer

--- common/optimizer.py
+++ common/optimizer.py
@@ -1,6 +1,6 @@
 from abc import *
 import numpy as np
-from typing import Union, List
+from typing import Any, Union, List
 
 from layer import Layer
 from activation import Activation
@@ -12,3 +12,6 @@ class Optimizer(metaclass=ABCMeta):
     @abstractmethod
     def step(self, x: np.ndarray, grad: float) -> None: 
         pass
+    
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        return self.step(*args, **kwds)
