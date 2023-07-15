769637ec27bbc86a33f94f024a2f61d0e3b27ec3
Yeho Gwon <devstita@gmail.com>
2023-07-16 02:59:07 +0900

    implement some loss & activation

--- common/loss.py
+++ common/loss.py
@@ -3,17 +3,27 @@ import numpy as np
 
 from typing import Any, Callable
 
-class Activation(metaclass=ABCMeta): 
+class Loss(metaclass=ABCMeta): 
     def __init__(self) -> None:
         pass
 
     @abstractmethod
-    def forward(self, x: np.ndarray) -> np.ndarray: 
+    def forward(self, y: np.ndarray, gt: np.ndarray) -> np.ndarray: 
         pass
 
     @abstractmethod
-    def backward(self, grad: float, optimizer: Callable) -> float: 
+    def backward(self, grad: np.ndarray, optimizer: Callable) -> float: 
         pass
 
     def __call__(self, *args: Any, **kwds: Any) -> Any:
         return self.forward(*args, **kwds)
+
+class MSE(Loss): 
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, y: np.ndarray, gt: np.ndarray) -> np.ndarray: 
+        return np.mean(np.sum((y - gt) ** 2, axis=1))
+
+    def backward(self, grad: np.ndarray, optimizer: Callable) -> float: 
+        return grad * 2 / np.prod(grad.shape)
