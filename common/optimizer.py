9e855dec66960355717a44b3ffa60050a835ec1b
Yeho Gwon <devstita@gmail.com>
2023-07-16 19:33:50 +0900

    implement SGD optimizer

--- common/optimizer.py
+++ common/optimizer.py
@@ -1,14 +1,25 @@
 from abc import *
 import numpy as np
-from typing import Any
+from typing import Any, Union, Tuple, List
+
+from array import GradArray
 
 class Optimizer(metaclass=ABCMeta): 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, *params: Union[Tuple[GradArray], List[GradArray]]) -> None:
+        self._params = params
         
     @abstractmethod
-    def step(self, x: np.ndarray, grad: float) -> None: 
+    def step(self) -> None: 
         pass
     
     def __call__(self, *args: Any, **kwds: Any) -> Any:
         return self.step(*args, **kwds)
+
+class SGD(Optimizer): 
+    def __init__(self, *params: Union[Tuple[GradArray], List[GradArray]],  lr: float) -> None:
+        super().__init__(*params)
+        self._lr = lr
+    
+    def step(self) -> None: 
+        for param in self._params:
+            param._array -= param.grad * self._lr
