ab9e2975793c67cff4164373dfcadff6c868a258
Yeho Gwon <devstita@gmail.com>
2023-07-17 15:03:24 +0900

    remove forward for Activation

--- common/activation.py
+++ common/activation.py
@@ -12,14 +12,18 @@ from common.array import GradArray, exp
 from common.layer import Layer
 
 class Activation(Layer, metaclass=ABCMeta): 
+    @abstractmethod
+    def op(self, x: GradArray) -> GradArray:
+        pass
+
+    def forward(self, x: np.ndarray) -> np.ndarray: 
+        self.x = x
+        self.out = self.op(x)
+        return self.out
+
     def get_params(self) -> List[GradArray]:
         return []
 
 class Sigmoid(Activation): 
     def op(self, x: GradArray) -> GradArray: 
         return 1 / (1 + exp(-x))
-
-    def forward(self, x: np.ndarray) -> np.ndarray: 
-        self.x = x
-        self.out = self.op(x)
-        return self.out
